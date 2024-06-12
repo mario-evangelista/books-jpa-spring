7eed1f18e4194967a2dc427fc77df5580ec17d67
Mário Evangelista <mariojbe@gmail.com>
2024-06-12 09:22:45 -0300

    @Transactional is invoked

--- src/main/java/net/mjdev/jpa/services/BookService.java
+++ src/main/java/net/mjdev/jpa/services/BookService.java
@@ -32,7 +32,13 @@ public class BookService {
         return bookRepository.findAll();
     }
 
-    @Transactional
+    /*
+    * @Transactional is invoked, a transaction is started before the method is executed and is
+    * finished at the end of the method. If the method execution is successful,
+    * the transaction is committed. If an exception occurs during method execution,
+    * the transaction is rolled back, ensuring that all operations performed within the transaction are undone.
+    * */
+    @Transactional //
     public BookModel saveBook(BookRecordDto bookRecordDto) {
         BookModel book = new BookModel();
         book.setTitle(bookRecordDto.title());
